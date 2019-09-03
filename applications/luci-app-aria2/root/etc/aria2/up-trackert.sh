ff39b3d76945f58ded310691926308e7ddae9cc6
ntlf9t <505575853@qq.com>
2019-09-03 21:09:28 +0800

    mod 3.1.56

--- applications/luci-app-aria2/root/etc/aria2/up-trackert.sh
+++ applications/luci-app-aria2/root/etc/aria2/up-trackert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /etc/init.d/aria2 stop
-list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt --no-check-certificate |awk NF|sed ":a;N;s/\n/,/g;ta"`
+list=`curl -s https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt |awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "option bt_tracker " /etc/config/aria2`" ]; then
     sed -i "$a option bt_tracker '"${list}"'" /etc/config/aria2
     echo add......
